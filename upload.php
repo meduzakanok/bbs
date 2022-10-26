6d738da1a1ab2f5c90e90c39bb0f8e789abeaafd
meduzakanok <116626938+meduzakanok@users.noreply.github.com>
2022-10-27 03:12:43 +0700

    Add files via upload

--- upload.php
+++ upload.php
@@ -4,7 +4,8 @@
     $current_date = current_date();
     extract($_POST);
 	$sql = "INSERT INTO login_session(login_user ,login_name, login_sname , login_date ) VALUES('" . $txtUser . "','" . $fname . "', '" . $lname . "', '" . $current_date . "')";
-    if(mysqli_query($con, $sql)) 
+	//$stmt = $con->query($sql);
+    if($con->query($sql)) 
 	{
 		//echo '1';
 		echo json_encode(array('success' => 1));
@@ -16,5 +17,5 @@
 		echo json_encode(array('success' => 0));
 		exit;
     }
-    mysqli_close($con);
+    //mysqli_close($con);
 ?>
