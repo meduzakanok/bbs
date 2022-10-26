028f1510545d527a9f55c02c20a401edb295db6f
meduzakanok <116626938+meduzakanok@users.noreply.github.com>
2022-10-26 23:10:32 +0700

    Add files via upload

---
+++ t_int1.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'landing/db_utf8.php';	
+include_once 'landing/function.php';
+
+echo "TEST connect DB <br/>";
+
+$stmt = $con->query("SELECT rec_name,rec_sname FROM rec_user");
+$stmt->execute();
+$result = $stmt->fetchAll();
+foreach ($result as $row){
+	echo "<li>".$row['rec_name']." ".$row['rec_sname']."</li>";
+}
+echo "<br>";
+$stmt = $con->query("SELECT * FROM sap_modules order by sap_module");
+while ($row = $stmt->fetch()) {
+    echo $row['sap_module'].'('.$row['SAP_moduleVal'].")<br />\n";  	
+}
+
+?>
