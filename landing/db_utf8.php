6e42989fc21cb2a5d9399b23c97a0da556cb5920
meduzakanok <116626938+meduzakanok@users.noreply.github.com>
2022-10-26 23:22:27 +0700

    Add files via upload

--- landing/db_utf8.php
+++ landing/db_utf8.php
@@ -1,8 +1,22 @@
 <?php
+/*
 $dbhost = "localhost";
 $dbuser = "bbsusr";
 $dbpass = "bbs#pwd";
 $dbname = "bbsrecruitdb";
 $con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die(mysql_error());
 mysqli_query($con,"set character set utf8");
+*/
+$cloud_sql_connection_name = "bbsrecruit:asia-southeast1:ttinst";
+$database = "bbsrecruitdb";
+$username = "bbsusr";
+$password = "bbs#pwd";
+$socket_dir = "/cloudsql";
+$dsn = sprintf(
+	'mysql:dbname=%s;unix_socket=%s/%s',
+	$database,
+	$socket_dir,
+	$cloud_sql_connection_name
+);
+$con = new PDO($dsn, $username,$password);
 ?>
