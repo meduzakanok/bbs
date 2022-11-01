c115bba27bb5e1a07d3b0517e2effa325a8d3d80
meduzakanok <116626938+meduzakanok@users.noreply.github.com>
2022-11-01 19:26:20 +0700

    Add files via upload

--- landing/db.php
+++ landing/db.php
@@ -13,7 +13,7 @@ $database = "bbsrecruitdb";
 $username = "bbsusr";
 $password = "bbs#pwd";
 $socket_dir = "/cloudsql";
-//$dsn = sprintf('mysql:dbname=%s;unix_socket=%s/%s',$database,$socket_dir,$cloud_sql_connection_name);
-$dsn = sprintf('mysql:dbname=%s;host=%s',$database,$dbhost);
+$dsn = sprintf('mysql:dbname=%s;unix_socket=%s/%s',$database,$socket_dir,$cloud_sql_connection_name);
+//$dsn = sprintf('mysql:dbname=%s;host=%s',$database,$dbhost);
 $con = new PDO($dsn, $username,$password);
 ?>
